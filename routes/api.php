78568cfa5e1ddd63977045785db22dbd891f67ba
unknown <seevaaluu@gmail.com>
2021-10-21 12:55:48 -0500

    login

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,19 @@ Route::get('/hola_mundo', function() {
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::group(['middleware' => 'auth'], function () {
+    // Routes libros
+    Route::get('/libros', 'ApiLibrosController@index')->name('api.libros.index');
+    Route::get('/libros/create', 'ApiLibrosController@create')->name('api.libros.create');
+    Route::post('/libros', 'ApiLibrosController@store')->name('api.libros.store');
+    Route::get('/libros/{id}/edit', 'ApiLibrosController@edit')->name('api.libros.edit');
+    Route::get('/libros/{id}', 'ApiLibrosController@show')->name('api.libros.show');
+    Route::patch('/libros/{id}', 'ApiLibrosController@update')->name('api.libros.update');
+    Route::delete('/libros/{id}', 'ApiLibrosController@destroy')->name('api.libros.destroy');
+
+});
+
+// Routes Authentiation
+Route::post('/auth/login', 'Auth\LoginController@api_login');
